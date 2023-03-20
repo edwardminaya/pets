4bc34b7c333805b57ae78c6fe52afe0c75333594
“Edward <eminaya49@gmail.com>
2023-03-20 14:39:57 -0600

    created edit and new actions

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,4 +6,32 @@ class PetsController < ApplicationController
   def show
     @pet = Pet.find(params[:id])
   end
+
+  def new
+    @pet = Pet.new
+    render :new
+  end
+
+  def create
+    @pet = Pet.create(
+      name: params[:pet][:name],
+      breed: params[:pet][:breed],
+    )
+    redirect_to "/pets"
+  end
+
+  def edit
+    @pet = Pet.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @pet = Pet.find(params[:id])
+    @pet.update(
+      name: params[:pet][:name],
+      breed: params[:pet][:breed],
+      image: params[:pet][:image],
+    )
+    redirect_to "/pets"
+  end
 end
