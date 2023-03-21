ab3e4ef31fc0daeabcd7572c74255cdae4cb0551
“Edward <eminaya49@gmail.com>
2023-03-21 08:24:51 -0600

    created sessions and login in views and controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   get "/signup", to: "users#new"
   post "/users", to: "users#create"
+
+  get "/login" => "sessions#new"
+  post "/sessions" => "sessions#create"
+  get "/logout" => "sessions#destroy"
 end
